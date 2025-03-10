7f0570a38719de092bdbfa6da06bc61d9eee7aab
Vyacheslavvvv <galin0ff@yandex.ru>
2025-03-10 16:41:01 +0300

    anotation + auto construct

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,6 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,11 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    private final CarServiceImpl carService = new CarServiceImpl();
+    private final CarServiceImpl carService;
+
+    public CarController(CarServiceImpl carService) {
+        this.carService = carService;
+    }
 
     @GetMapping(value = "/cars")
     public String cars(
